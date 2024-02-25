bbcd18ed21651d5d6fba65deb67b169ff8186a3f
louis <louis@192.168.0.16>
2024-02-26 01:42:36 +0800

    編譯環境調整

--- build.sh
+++ build.sh
@@ -1,8 +1,14 @@
+IP=ip addr |grep inet|grep -v 127.0.0.1|grep -v inet6|awk '{print $2}'|grep -v 172| cut -d'/' -f 1
 SINOPAC_ROOT=/usr/AP
 SINOPAC_PATH=${SINOPAC_ROOT}/sinopac
 SINOPAC_LOG_PATH=${SINOPAC_PATH}/logs
+ENV=prod
 
 PID=$(ps -ef | grep java | grep api | awk '{print $2}')
+
+[ $IP = "10.1.129.11" ] && ENV="dev"
+
+echo this environment is [${ENV}]
 
 echo rebooting please wait...
 
@@ -22,7 +28,7 @@ sudo touch ${SINOPAC_LOG_PATH}/info.log
 cd ${SINOPAC_PATH}/target
 
 sudo nohup java \
-	 -jar api.jar --spring.profiles.active=dev \
+	 -jar api.jar --spring.profiles.active=${ENV} \
 	 > /dev/null 2>&1 &
 	 
 #tail -f ${SINOPAC_LOG_PATH}/info.log
